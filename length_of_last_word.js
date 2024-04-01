1b98c9ce48686b36121608f092062a7d79389193
Alice Déchevaux <alicedechevaux@gmail.com>
2024-04-01 10:17:04 -0500

    return the length of the final word in a string

---
+++ length_of_last_word.js
@@ -0,0 +1,53 @@
+//04/01/2024
+
+// Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+// A word is a maximal
+// substring
+// consisting of non-space characters only.
+
+ 
+
+// Example 1:
+
+// Input: s = "Hello World"
+// Output: 5
+// Explanation: The last word is "World" with length 5.
+
+// Example 2:
+
+// Input: s = "   fly me   to   the moon  "
+// Output: 4
+// Explanation: The last word is "moon" with length 4.
+
+// Example 3:
+
+// Input: s = "luffy is still joyboy"
+// Output: 6
+// Explanation: The last word is "joyboy" with length 6.
+
+ 
+
+// Constraints:
+
+//     1 <= s.length <= 104
+//     s consists of only English letters and spaces ' '.
+//     There will be at least one word in s.
+
+var lengthOfLastWord = function(s) {
+    let length = 0;
+    for (let i = s.length - 1; i >= 0; i--) {
+        if (s[i] !== " ") length++;
+        if (length > 0 && s[i] === " ") break;
+    }
+    return length;
+};
+
+//or 
+
+var lengthOfLastWord = function(s) {
+    s = s.trim().split(" ")
+    return s[s.length - 1].length
+};
+
+//This second solution was actually faster which surprised me
