d2a18d62716980f88a2bd2f17f5378277450caa4
Alice Déchevaux <alicedechevaux@gmail.com>
2024-04-21 17:59:49 -0500

    determine how many words can be formed from a string of letters

---
+++ find_words_that_can_be_formed_by_characters.js
@@ -0,0 +1,52 @@
+//04/21/2024
+
+// You are given an array of strings words and a string chars.
+
+// A string is good if it can be formed by characters from chars (each character can only be used once).
+
+// Return the sum of lengths of all good strings in words.
+
+ 
+
+// Example 1:
+
+// Input: words = ["cat","bt","hat","tree"], chars = "atach"
+// Output: 6
+// Explanation: The strings that can be formed are "cat" and "hat" so the answer is 3 + 3 = 6.
+
+// Example 2:
+
+// Input: words = ["hello","world","leetcode"], chars = "welldonehoneyr"
+// Output: 10
+// Explanation: The strings that can be formed are "hello" and "world" so the answer is 5 + 5 = 10.
+
+ 
+
+// Constraints:
+
+//     1 <= words.length <= 1000
+//     1 <= words[i].length, chars.length <= 100
+//     words[i] and chars consist of lowercase English letters.
+
+var countCharacters = function(words, chars) {
+    let temp, match, total = 0;
+
+    for (let i = 0; i < words.length; i++) {
+        temp = [...chars];
+        console.log(temp)
+        match = true;
+        for (let j = 0; j < words[i].length; j++) {
+            if (temp.includes(words[i][j])) {
+                temp[temp.indexOf(words[i][j])] = "";
+            }
+            else {
+                match = false;
+            }
+        }
+        if (match) {
+            console.log(words[i])
+            total += words[i].length;
+        }
+    }
+    return total;
+};
