664f04698876f6578ea16312876714ab4b049584
Alice Déchevaux <alicedechevaux@gmail.com>
2024-04-28 17:47:46 -0500

    return true if a linked list represents a palindrome and false otherwise

---
+++ palindrome_linked_list.js
@@ -0,0 +1,36 @@
+//04/28/2024
+
+// Given the head of a singly linked list, return true if it is a
+// palindrome
+// or false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: head = [1,2,2,1]
+// Output: true
+
+// Example 2:
+
+// Input: head = [1,2]
+// Output: false
+
+ 
+
+// Constraints:
+
+//     The number of nodes in the list is in the range [1, 105].
+//     0 <= Node.val <= 9
+
+ 
+// Follow up: Could you do it in O(n) time and O(1) space?
+
+var isPalindrome = function(head) {
+    let result = [];
+    while (head) {
+        result.push(head.val);
+        head = head.next;
+    }
+    return result.join("") === result.reverse().join("");
+};
