ab69dd88a3ae78157381186b21fca2df23adc565
Alice Déchevaux <alicedechevaux@gmail.com>
2024-05-11 08:26:44 -0500

    test a string for valid combinations of opening and closing parentheses

---
+++ valid_parentheses.js
@@ -0,0 +1,57 @@
+//05/11/2024
+
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+//     Open brackets must be closed by the same type of brackets.
+//     Open brackets must be closed in the correct order.
+//     Every close bracket has a corresponding open bracket of the same type.
+
+ 
+
+// Example 1:
+
+// Input: s = "()"
+// Output: true
+
+// Example 2:
+
+// Input: s = "()[]{}"
+// Output: true
+
+// Example 3:
+
+// Input: s = "(]"
+// Output: false
+
+ 
+
+// Constraints:
+
+//     1 <= s.length <= 104
+//     s consists of parentheses only '()[]{}'.
+
+var isValid = function(s) {
+    let stack = [];
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "(") stack.push("(");
+        if (s[i] === "{") stack.push("{");
+        if (s[i] === "[") stack.push("[");
+        if (s[i] === ")") {
+            if (stack[stack.length - 1] !== "(") return false;
+            else stack.pop();
+        }
+        if (s[i] === "}") {
+            if (stack[stack.length - 1] !== "{") return false;
+            else stack.pop();
+        }
+        if (s[i] === "]") {
+            if (stack[stack.length - 1] !== "[") return false;
+            else stack.pop();
+        }
+    }
+    if (stack.length) return false;
+    else return true;
+};
