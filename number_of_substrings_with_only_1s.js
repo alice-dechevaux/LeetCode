87fb996c2e43422f0e7f35a8480f07ba76402334
Alice Déchevaux <alicedechevaux@gmail.com>
2024-05-30 08:17:48 -0500

    given a binary string return the total number of substrings containing only ones

---
+++ number_of_substrings_with_only_1s.js
@@ -0,0 +1,50 @@
+//05/30/2024
+
+// Given a binary string s, return the number of substrings with all characters 1's. Since the answer may be too large, return it modulo 109 + 7.
+
+ 
+
+// Example 1:
+
+// Input: s = "0110111"
+// Output: 9
+// Explanation: There are 9 substring in total with only 1's characters.
+// "1" -> 5 times.
+// "11" -> 3 times.
+// "111" -> 1 time.
+
+// Example 2:
+
+// Input: s = "101"
+// Output: 2
+// Explanation: Substring "1" is shown 2 times in s.
+
+// Example 3:
+
+// Input: s = "111111"
+// Output: 21
+// Explanation: Each substring contains only 1's characters.
+
+ 
+
+// Constraints:
+
+//     1 <= s.length <= 105
+//     s[i] is either '0' or '1'.
+
+var numSub = function(s) {
+    let current, total = 0, mod = 10 ** 9 + 7;
+    for (let i = 0; i < s.length; i++) {
+        current = 0;
+        if (s[i] === "1") {
+            current++;
+            for (let j = i + 1; j < s.length; j++) {
+                if(s[j] === "1") current++;
+                else break;
+            }
+            i += current - 1;
+            total += ((current + 1) * current / 2) % mod;
+        }
+    }
+    return total;
+};
